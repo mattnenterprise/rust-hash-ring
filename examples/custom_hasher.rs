70185c53576debf3bbe79b11c5d29965ff86b957
Matt McCoy <mattnenterprise@yahoo.com>
2020-12-13 18:39:37 -0500

    Switch to XXHash64 over MD5 and allow configurable hash function

---
+++ examples/custom_hasher.rs
@@ -0,0 +1,42 @@
+extern crate hash_ring;
+
+use std::hash::Hasher;
+use std::hash::BuildHasherDefault;
+use hash_ring::HashRing;
+use hash_ring::NodeInfo;
+
+// This is a hasher that always returns the same number
+// no matter the input. This is meant as an example and
+// should never be used in production code as all keys go
+// to the same node.
+#[derive(Default)]
+struct ConstantHasher;
+
+impl Hasher for ConstantHasher {
+    fn write(&mut self, _bytes: &[u8]) {
+        // Do nothing
+    }
+
+    fn finish(&self) -> u64 {
+        return 1; 
+    }
+}
+
+type ConstantBuildHasher = BuildHasherDefault<ConstantHasher>;
+
+fn main() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 15324});
+    nodes.push(NodeInfo{host: "localhost", port: 15325});
+    nodes.push(NodeInfo{host: "localhost", port: 15326});
+    nodes.push(NodeInfo{host: "localhost", port: 15327});
+    nodes.push(NodeInfo{host: "localhost", port: 15328});
+    nodes.push(NodeInfo{host: "localhost", port: 15329});
+
+    let hash_ring: HashRing<NodeInfo, ConstantBuildHasher> = HashRing::with_hasher(nodes, 10, ConstantBuildHasher::default());
+
+    println!("Key: '{}', Node: {}", "hello", hash_ring.get_node(("hello").to_string()).unwrap());
+    println!("Key: '{}', Node: {}", "dude", hash_ring.get_node(("dude").to_string()).unwrap());
+    println!("Key: '{}', Node: {}", "martian", hash_ring.get_node(("martian").to_string()).unwrap());
+    println!("Key: '{}', Node: {}", "tardis", hash_ring.get_node(("tardis").to_string()).unwrap());
+}
--- src/hash_ring.rs
+++ src/hash_ring.rs
@@ -1,6 +1,10 @@
-use md5;
+use std::hash::BuildHasher;
+use std::hash::BuildHasherDefault;
+use std::hash::Hasher;
 use std::collections::HashMap;
 use std::collections::BinaryHeap;
+use std::fmt::{self};
+use twox_hash::XxHash64;
 
 /// As a convenience, rust-hash-ring provides a default struct to hold node
 /// information. It is optional and you can define your own.
@@ -10,27 +14,42 @@ pub struct NodeInfo {
     pub port: u16,
 }
 
-impl ToString for NodeInfo {
-    fn to_string(&self) -> String {
-        format!("{}:{}", self.host, self.port)
+impl fmt::Display for NodeInfo {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(fmt, "{}:{}", self.host, self.port)
     }
 }
 
+type XxHash64Hasher = BuildHasherDefault<XxHash64>;
+
 /// HashRing
-pub struct HashRing<T> {
+pub struct HashRing<T, S = XxHash64Hasher> {
     replicas: isize,
-    ring: HashMap<String, T>,
-    sorted_keys: Vec<String>,
+    ring: HashMap<u64, T>,
+    sorted_keys: Vec<u64>,
+    hash_builder: S,
 }
 
-impl<T: ToString + Clone> HashRing<T> {
+impl<T: ToString + Clone> HashRing<T, XxHash64Hasher> {
     /// Creates a new hash ring with the specified nodes.
     /// Replicas is the number of virtual nodes each node has to make a better distribution.
-    pub fn new(nodes: Vec<T>, replicas: isize) -> HashRing<T> {
-        let mut new_hash_ring: HashRing<T> = HashRing {
+    pub fn new(nodes: Vec<T>, replicas: isize) -> HashRing<T, XxHash64Hasher> {
+        return HashRing::with_hasher(nodes, replicas, XxHash64Hasher::default());
+    }
+}
+
+impl<T, S> HashRing<T, S>
+where
+    T: ToString + Clone,
+    S: BuildHasher,
+{
+
+    pub fn with_hasher(nodes: Vec<T>, replicas: isize, hash_builder: S) -> HashRing<T, S> {
+        let mut new_hash_ring: HashRing<T, S> = HashRing {
             replicas: replicas,
             ring: HashMap::new(),
             sorted_keys: Vec::new(),
+            hash_builder: hash_builder,
         };
 
         for i in 0..nodes.len() {
@@ -91,14 +110,17 @@ impl<T: ToString + Clone> HashRing<T> {
     }
 
     /// Generates a key from a string value
-    fn gen_key(&self, key: String) -> String {
-        let digest = md5::compute(key);
-        format!("{:x}", digest)
+    fn gen_key(&self, key: String) -> u64 {
+        let mut hasher = self.hash_builder.build_hasher();
+        hasher.write(key.as_bytes());
+        return hasher.finish();
     }
 }
 
 #[cfg(test)]
 mod test {
+    use std::hash::Hasher;
+    use std::hash::BuildHasherDefault;
     use hash_ring::{NodeInfo, HashRing};
 
     // Defines a NodeInfo for a localhost address with a given port.
@@ -125,17 +147,17 @@ mod test {
         nodes.push(node(15328));
         nodes.push(node(15329));
 
-
         let mut hash_ring: HashRing<NodeInfo> = HashRing::new(nodes, 10);
 
-        assert_eq!(Some(&node(15329)), hash_ring.get_node("hello".to_string()));
-        assert_eq!(Some(&node(15326)), hash_ring.get_node("dude".to_string()));
+        assert_eq!(Some(&node(15326)), hash_ring.get_node("hello".to_string()));
+        assert_eq!(Some(&node(15327)), hash_ring.get_node("dude".to_string()));
+        assert_eq!(Some(&node(15324)), hash_ring.get_node("two".to_string()));
 
         hash_ring.remove_node(&node(15329));
-        assert_eq!(Some(&node(15327)), hash_ring.get_node("hello".to_string()));
+        assert_eq!(Some(&node(15326)), hash_ring.get_node("hello".to_string()));
 
         hash_ring.add_node(&node(15329));
-        assert_eq!(Some(&node(15329)), hash_ring.get_node("hello".to_string()));
+        assert_eq!(Some(&node(15326)), hash_ring.get_node("hello".to_string()));
     }
 
     #[derive(Clone)]
@@ -153,7 +175,6 @@ mod test {
 
     #[test]
     fn test_custom_nodes() {
-
         let mut nodes: Vec<CustomNodeInfo> = Vec::new();
         nodes.push(CustomNodeInfo {
             host: "localhost",
@@ -183,13 +204,13 @@ mod test {
         let mut hash_ring: HashRing<CustomNodeInfo> = HashRing::new(nodes, 10);
 
         assert_eq!(
-            Some("localhost:15329".to_string()),
+            Some("localhost:15326".to_string()),
             hash_ring.get_node("hello".to_string()).map(
                 |x| x.to_string(),
             )
         );
         assert_eq!(
-            Some("localhost:15326".to_string()),
+            Some("localhost:15327".to_string()),
             hash_ring.get_node("dude".to_string()).map(
                 |x| x.to_string(),
             )
@@ -200,7 +221,7 @@ mod test {
             port: 15329,
         });
         assert_eq!(
-            Some("localhost:15327".to_string()),
+            Some("localhost:15326".to_string()),
             hash_ring.get_node("hello".to_string()).map(
                 |x| x.to_string(),
             )
@@ -211,12 +232,11 @@ mod test {
             port: 15329,
         });
         assert_eq!(
-            Some("localhost:15329".to_string()),
+            Some("localhost:15326".to_string()),
             hash_ring.get_node("hello".to_string()).map(
                 |x| x.to_string(),
             )
         );
-
     }
 
     #[test]
@@ -259,5 +279,37 @@ mod test {
         // This should be num nodes * num replicas
         assert_eq!(60, hash_ring.sorted_keys.len());
         assert_eq!(60, hash_ring.ring.len());
+    }
+
+    #[test]
+    fn test_custom_hasher() {
+        #[derive(Default)]
+        struct ConstantHasher;
+
+        impl Hasher for ConstantHasher {
+            fn write(&mut self, _bytes: &[u8]) {
+                // Do nothing
+            }
+
+            fn finish(&self) -> u64 {
+                return 1;
+            }
+        }
+
+        type ConstantBuildHasher = BuildHasherDefault<ConstantHasher>;
+
+        let mut nodes: Vec<NodeInfo> = Vec::new();
+        nodes.push(node(15324));
+        nodes.push(node(15325));
+        nodes.push(node(15326));
+        nodes.push(node(15327));
+        nodes.push(node(15328));
+        nodes.push(node(15329));
+
+        let hash_ring: HashRing<NodeInfo, ConstantBuildHasher> = HashRing::with_hasher(nodes, 10, ConstantBuildHasher::default());
+
+        assert_eq!(Some(&node(15329)), hash_ring.get_node("hello".to_string()));
+        assert_eq!(Some(&node(15329)), hash_ring.get_node("dude".to_string()));
+        assert_eq!(Some(&node(15329)), hash_ring.get_node("two".to_string()));
     }
 }
