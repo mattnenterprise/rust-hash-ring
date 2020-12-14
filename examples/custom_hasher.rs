acc0492b110cc407007df297593e85cf7a201e1f
Matt McCoy <mattnenterprise@yahoo.com>
2020-12-13 19:09:56 -0500

    Format examples and update README usage section

--- examples/custom_hasher.rs
+++ examples/custom_hasher.rs
@@ -1,9 +1,9 @@
 extern crate hash_ring;
 
-use std::hash::Hasher;
-use std::hash::BuildHasherDefault;
 use hash_ring::HashRing;
 use hash_ring::NodeInfo;
+use std::hash::BuildHasherDefault;
+use std::hash::Hasher;
 
 // This is a hasher that always returns the same number
 // no matter the input. This is meant as an example and
@@ -18,7 +18,7 @@ impl Hasher for ConstantHasher {
     }
 
     fn finish(&self) -> u64 {
-        return 1; 
+        return 1;
     }
 }
 
@@ -26,17 +26,52 @@ type ConstantBuildHasher = BuildHasherDefault<ConstantHasher>;
 
 fn main() {
     let mut nodes: Vec<NodeInfo> = Vec::new();
-    nodes.push(NodeInfo{host: "localhost", port: 15324});
-    nodes.push(NodeInfo{host: "localhost", port: 15325});
-    nodes.push(NodeInfo{host: "localhost", port: 15326});
-    nodes.push(NodeInfo{host: "localhost", port: 15327});
-    nodes.push(NodeInfo{host: "localhost", port: 15328});
-    nodes.push(NodeInfo{host: "localhost", port: 15329});
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15324,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15325,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15326,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15327,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15328,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15329,
+    });
 
-    let hash_ring: HashRing<NodeInfo, ConstantBuildHasher> = HashRing::with_hasher(nodes, 10, ConstantBuildHasher::default());
+    let hash_ring: HashRing<NodeInfo, ConstantBuildHasher> =
+        HashRing::with_hasher(nodes, 10, ConstantBuildHasher::default());
 
-    println!("Key: '{}', Node: {}", "hello", hash_ring.get_node(("hello").to_string()).unwrap());
-    println!("Key: '{}', Node: {}", "dude", hash_ring.get_node(("dude").to_string()).unwrap());
-    println!("Key: '{}', Node: {}", "martian", hash_ring.get_node(("martian").to_string()).unwrap());
-    println!("Key: '{}', Node: {}", "tardis", hash_ring.get_node(("tardis").to_string()).unwrap());
+    println!(
+        "Key: '{}', Node: {}",
+        "hello",
+        hash_ring.get_node(("hello").to_string()).unwrap()
+    );
+    println!(
+        "Key: '{}', Node: {}",
+        "dude",
+        hash_ring.get_node(("dude").to_string()).unwrap()
+    );
+    println!(
+        "Key: '{}', Node: {}",
+        "martian",
+        hash_ring.get_node(("martian").to_string()).unwrap()
+    );
+    println!(
+        "Key: '{}', Node: {}",
+        "tardis",
+        hash_ring.get_node(("tardis").to_string()).unwrap()
+    );
 }
--- examples/readme.rs
+++ examples/readme.rs
@@ -5,28 +5,76 @@ use hash_ring::NodeInfo;
 
 fn main() {
     let mut nodes: Vec<NodeInfo> = Vec::new();
-    nodes.push(NodeInfo{host: "localhost", port: 15324});
-    nodes.push(NodeInfo{host: "localhost", port: 15325});
-    nodes.push(NodeInfo{host: "localhost", port: 15326});
-    nodes.push(NodeInfo{host: "localhost", port: 15327});
-    nodes.push(NodeInfo{host: "localhost", port: 15328});
-    nodes.push(NodeInfo{host: "localhost", port: 15329});
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15324,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15325,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15326,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15327,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15328,
+    });
+    nodes.push(NodeInfo {
+        host: "localhost",
+        port: 15329,
+    });
 
     let mut hash_ring: HashRing<NodeInfo> = HashRing::new(nodes, 10);
 
-    println!("Key: '{}', Node: {}", "hello" , hash_ring.get_node(("hello").to_string()).unwrap());
+    println!(
+        "Key: '{}', Node: {}",
+        "hello",
+        hash_ring.get_node(("hello").to_string()).unwrap()
+    );
 
-    println!("Key: '{}', Node: {}", "dude", hash_ring.get_node(("dude").to_string()).unwrap());
+    println!(
+        "Key: '{}', Node: {}",
+        "dude",
+        hash_ring.get_node(("dude").to_string()).unwrap()
+    );
 
-    println!("Key: '{}', Node: {}", "martian", hash_ring.get_node(("martian").to_string()).unwrap());
+    println!(
+        "Key: '{}', Node: {}",
+        "martian",
+        hash_ring.get_node(("martian").to_string()).unwrap()
+    );
 
-    println!("Key: '{}', Node: {}", "tardis", hash_ring.get_node(("tardis").to_string()).unwrap());
+    println!(
+        "Key: '{}', Node: {}",
+        "tardis",
+        hash_ring.get_node(("tardis").to_string()).unwrap()
+    );
 
-    hash_ring.remove_node(&NodeInfo{host: "localhost", port: 15329});
+    hash_ring.remove_node(&NodeInfo {
+        host: "localhost",
+        port: 15329,
+    });
 
-    println!("Key: '{}', Node: {}", "hello", hash_ring.get_node(("hello").to_string()).unwrap());
+    println!(
+        "Key: '{}', Node: {}",
+        "hello",
+        hash_ring.get_node(("hello").to_string()).unwrap()
+    );
 
-    hash_ring.add_node(&NodeInfo{host: "localhost", port: 15329});
+    hash_ring.add_node(&NodeInfo {
+        host: "localhost",
+        port: 15329,
+    });
 
-    println!("Key: '{}', Node: {}", "hello", hash_ring.get_node(("hello").to_string()).unwrap());
+    println!(
+        "Key: '{}', Node: {}",
+        "hello",
+        hash_ring.get_node(("hello").to_string()).unwrap()
+    );
 }
